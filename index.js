48ad308c99502c31e5e581bd11391bda4cdf5b04
LucassantoSCruz <lucas.cruz162@etec.sp.gov.br>
2023-03-26 15:23:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 
 const routeCategoria = require('./route/routeCategoria')
-
 const routeMarca = require('./route/routeMarca')
+const routeFornecedor = require('./route/routeFornecedor')
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true})); // adicionei essa linha
 
 app.use('/', routeCategoria);
-
-app.use('/', routeMarca)
+app.use('/', routeMarca);
+app.use('/', routeFornecedor)
 
 app.listen(3000, ()=>{
     console.log('O SERVIDOR ESTÁ LIGADO EM - http://localhost:3000')
